2329de78379cf3218d4e5bb0d048a84a94635a48
Isabella <aarizonastrange@gmail.com>
2024-09-24 14:49:35 +0000

    tchetchereretchetche

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,5 +1,29 @@
 @extends('layout.app')
-@section('title', 'home')
+<!-- Passar o nome da pagina para o titulo -->
+@section('title', 'Home')
+
+<!-- inserir no yield do app sheet este fragmento -->
 @section('content')
-    <h1>Loja Sengés</h1>
-    @endsection
+<div class="row">
+    @foreach($produtos as $produto)
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+        <div class="card h-100">
+            <img src="{{$produto->imagem}}" alt="{{$produto->nome}}"
+                class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">
+                    {{ Str::limit($produto->nome,30)}}
+                </h5>
+                <p class="card-text">
+                    {{ Str::limit($produto->nome,50)}}
+                </p>
+                <a href="/site/produtos/{{$produto->slug}}"
+                    class="btn btn-primary">
+                    <i class="bi bi-eye">Ver</i>
+                </a>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
+@endsectionjvu
